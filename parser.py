07c8777d65ed7288a4d974d0508f926bf28b0049
Chvalov <andrii.chvalov@gmail.com>
2024-01-22 23:25:37 +0200

    Update parser.py
    parser fixes

--- parser.py
+++ parser.py
@@ -5,8 +5,8 @@ import xml.etree.ElementTree as ET
 tree = ET.parse(sys.argv[1])
 root = tree.getroot()
 
-for data in root.iter('address'):
-    ip = data.get('addr')
-    for data in root.iter('port'):
-        port = data.get('portid')
+for data in root.iter('host'):
+    ip = data.find('address').get('addr')
+    for ports in data.iter('port'):
+        port = ports.get('portid')
     print(ip + ':' + port)
